c70067762221d63207d7a221f059cbcaa52130ad
Matt Ruder <matt.j.ruder@gmail.com>
2023-05-25 13:51:22 -0500

    add basic module exports

---
+++ data/users.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+        username: 'matt',
+        password: 'bobatea23'
+    },
+    {
+        username: 'lindsey',
+        password: 'beyonce9'
+    }
+]
+
+module.exports = users
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
+var users = require('./data/users.js') 
 
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
+
+   app.get("/api/people", (req, res, next) => {
+    res.json(users);
+   });
+
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
