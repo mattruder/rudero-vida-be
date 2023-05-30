4940867a68c448449d21670bdcb7038271d629a0
Matt Ruder <matt.j.ruder@gmail.com>
2023-05-30 12:44:59 -0500

    updates

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ var express = require("express");
 var app = express();
 var users = require('./data/users.js') 
 
+
+
+
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 
    app.get("/api/people", (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
     res.json(users);
    });
 
 
-app.listen(3001, () => {
- console.log("Server running on port 3001");
+app.listen(4000, () => {
+ console.log("Server running on port 4000");
 });
+
